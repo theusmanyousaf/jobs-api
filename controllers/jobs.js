3abfcfeb14441520abdf62a15b0fc471646f295b
Usman Yousaf <officialusmanyousaf@gmail.com>
2024-10-21 23:40:23 +0500

    create job route logic

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,25 +1,26 @@
 const Job = require('../models/Job')
-const StatusCodes = require('http-status-codes')
+const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, NotFoundError } = require('../errors')
 
-const getAllJobs = (req,res) => {
+const getAllJobs = (req, res) => {
     res.send("get all jobs")
 }
 
-const getJob = (req,res) => {
+const getJob = (req, res) => {
     res.send("get job")
 }
 
-const createJob = (req,res) => {
-    
-    res.send(req.body)
+const createJob = async (req, res) => {
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({ job })
 }
 
-const updatedJob = (req,res) => {
+const updatedJob = (req, res) => {
     res.send("updated jobs")
 }
 
-const deleteJob = (req,res) => {
+const deleteJob = (req, res) => {
     res.send("delete jobs")
 }
 
